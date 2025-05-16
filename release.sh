8e3aecad44c622087ad32b8ed31733281c2ff3e3
SemkiShow <mgdeveloper123@gmail.com>
2025-05-16 10:18:29 +0200

    Make the Windows build always static

--- release.sh
+++ release.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
+
+set -e
+
 # Compiling for Linux
-mkdir build
-cd build &&
-cmake .. -DCMAKE_BUILD_TYPE=Release &&
-make &&
+if [ ! -d build ]; then
+    mkdir build
+fi
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make
 cd ..
 cp build/bin/main main
 
 # Compiling for Windows
-mkdir build_win
-cd build_win &&
-cmake .. -DCMAKE_BUILD_TYPE=Release --toolchain ../mingw-w64-x86_64.cmake &&
-make &&
+if [ ! -d build_win ]; then
+    mkdir build_win
+fi
+cd build_win
+cmake .. -DCMAKE_BUILD_TYPE=Release --toolchain ../mingw-w64-x86_64.cmake
+make
 cd ..
 cp build_win/bin/main.exe .
 
 # Zipping the build
 rm release.zip
 printf "vsync=true\n" > settings.txt
-zip release.zip main main.exe libgcc_s_seh-1.dll libstdc++-6.dll assets/* LICENSE README.md settings.txt
+zip release.zip main main.exe assets/* LICENSE README.md settings.txt
 rm main main.exe
-
-# Making a new GitHub commit
-git add .
-git commit
-git push origin main
 
 # Creating a GitHub release
 gh release create $1 release.zip
